3e94c1af0a9bbcac74b75c12a9da09480d7ece2d
andreytantsyura <andreytancyura@gmail.com>
2021-12-01 11:12:35 +0300

    Save

---
+++ src/InterTwitter/Resources/Styles/DefaultResources.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace InterTwitter.Resources.Styles
+{
+    public partial class DefaultResources : ResourceDictionary
+    {
+        public DefaultResources()
+        {
+            InitializeComponent();
+        }
+    }
+}
