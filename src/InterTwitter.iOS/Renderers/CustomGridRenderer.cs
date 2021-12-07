263b5df2eff9b0a1b1ef0275e4cb648aa1c8bd49
Denys Vedernykov <razom.info@gmail.com>
2021-12-07 14:07:17 +0200

    fixed

---
+++ src/InterTwitter.iOS/Renderers/CustomGridRenderer.cs
@@ -0,0 +1,87 @@
+﻿using CoreGraphics;
+using Foundation;
+using InterTwitter.Controls;
+using InterTwitter.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomGrid), typeof(CustomGridRenderer))]
+namespace InterTwitter.iOS.Renderers
+{
+    class CustomGridRenderer : ViewRenderer
+    {
+        NSObject _keyboardShowObserver;
+        NSObject _keyboardHideObserver;
+
+        #region -- Overrides --
+
+        protected override void OnElementChanged(ElementChangedEventArgs<View> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                RegisterForKeyboardNotifications();
+            }
+
+            if (e.OldElement != null)
+            {
+                UnregisterForKeyboardNotifications();
+            }
+        }
+
+        #endregion
+
+        #region -- Private Helpers --
+
+        private void RegisterForKeyboardNotifications()
+        {
+            if (_keyboardShowObserver == null)
+            {
+                _keyboardShowObserver = UIKeyboard.Notifications.ObserveWillShow(OnKeyboardShow);
+            }
+                
+            if (_keyboardHideObserver == null)
+            {
+                _keyboardHideObserver = UIKeyboard.Notifications.ObserveWillHide(OnKeyboardHide);
+            }
+        }
+
+        private void OnKeyboardShow(object sender, UIKeyboardEventArgs args)
+        {
+            NSValue result = (NSValue)args.Notification.UserInfo.ObjectForKey(new NSString(UIKeyboard.FrameEndUserInfoKey));
+            CGSize keyboardSize = result.RectangleFValue.Size;
+
+            if (Element != null)
+            {
+                Element.Margin = new Thickness(0, 0, 0, keyboardSize.Height - 35);
+            }
+        }
+
+        private void OnKeyboardHide(object sender, UIKeyboardEventArgs args)
+        {
+            if (Element != null)
+            {
+                Element.Margin = new Thickness(0);
+            }
+        }
+
+        private void UnregisterForKeyboardNotifications()
+        {
+            if (_keyboardShowObserver != null)
+            {
+                _keyboardShowObserver.Dispose();
+                _keyboardShowObserver = null;
+            }
+
+            if (_keyboardHideObserver != null)
+            {
+                _keyboardHideObserver.Dispose();
+                _keyboardHideObserver = null;
+            }
+        }
+
+        #endregion
+    }
+}
